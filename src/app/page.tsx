826d1dd250ba5816afb4cb1cea5f7c2286f2a062
Harsh Tiwari <harsh.official1802@gmail.com>
2025-11-04 22:38:51 +0530

    TRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
 
-export default function Home() {
+export default async function Page() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
   return (
     <div>
-      <Button variant="ghost">Click Me</Button>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Client />
+      </HydrationBoundary>
     </div>
   );
 }
