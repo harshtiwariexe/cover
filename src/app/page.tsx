15336686009d75bd96f8f20b5bf83398aa975538
Harsh Tiwari <harsh.official1802@gmail.com>
2025-11-08 17:30:52 +0530

    Inngest Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
+"use client";
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
 
-export const Page = async () => {
-  await requireAuth();
-  const data = await caller.getUsers();
+export const Page = () => {
+  // await requireAuth();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
+
+  // const options = trpc.getWorkflows.queryOptions();
+  // console.log("queryOptions:", options);
+  // const { data, error, isLoading } = useQuery(options);
+  // if (error) console.error(error);
 
   return (
-    <div>
+    <div className="flex flex-col gap-6">
       Protected Componnts
-      {JSON.stringify(data)}
+      {JSON.stringify(data, null, 2)}
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,29 @@
-import { z } from "zod";
+import { email, z } from "zod";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+import { inngest } from "@/inngest/client";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    console.log({ userID: ctx.auth.user.id });
+  getWorkflows: protectedProcedure.query(async ({ ctx }) => {
+    // If workflows are user-specific, filter by ctx.auth.user.id
+    // return prisma.workflow.findMany({ where: { userId: ctx.auth.user.id } });
+    return prisma.workflow.findMany();
+  }),
 
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  createWorkflow: protectedProcedure.mutation(async ({ ctx }) => {
+    // consider user-specific creation:
+    // return prisma.workflow.create({
+    //   data: { name: "Test Workflow", userId: ctx.auth.user.id },
+    // });
+
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "harsh@gmail.com",
       },
     });
+
+    return { success: true, message: "Job Quued" };
   }),
 });
 
