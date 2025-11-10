a966e11d71ad59fdf20e7294f73d3758c8246109
Harsh Tiwari <harsh.official1802@gmail.com>
2025-11-11 02:28:22 +0530

    Sidebar & Header Layout

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,140 @@
+"use client";
+import {
+  CreditCardIcon,
+  FolderOpenIcon,
+  HistoryIcon,
+  KeyIcon,
+  LogOutIcon,
+  StarIcon,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+
+const menuItems = [
+  {
+    title: "Main",
+    items: [
+      {
+        title: "Workflows",
+        icon: FolderOpenIcon,
+        url: "/workflows",
+      },
+      {
+        title: "Credentials",
+        icon: KeyIcon,
+        url: "/credentials",
+      },
+      {
+        title: "Executions",
+        icon: HistoryIcon,
+        url: "/executions",
+      },
+    ],
+  },
+];
+
+const AppSidebar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenuItem>
+          <SidebarMenuButton asChild className="gap-x-4 h-10 px-4">
+            <Link
+              href="/workflows"
+              prefetch
+              className="flex items-center justify-center"
+            >
+              <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarHeader>
+      <SidebarContent>
+        {menuItems.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton
+                      tooltip={item.title}
+                      isActive={
+                        item.url === "/"
+                          ? pathname === "/"
+                          : pathname.startsWith(item.url)
+                      }
+                      asChild
+                      className="gap-x-4 h-10 px-4"
+                    >
+                      <Link href={item.url} prefetch>
+                        <item.icon className="size-4" />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            tooltip="Upgrade to Pro"
+            onClick={() => {}}
+            className="gap-x-4 h-10 px-4"
+          >
+            <StarIcon className="h-4 w-4" />
+            <span>Upgrade to Pro</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            tooltip="Billing Portal"
+            onClick={() => {}}
+            className="gap-x-4 h-10 px-4"
+          >
+            <CreditCardIcon className="h-4 w-4" />
+            <span>Billing Portal</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            tooltip="Sign Out"
+            onClick={() =>
+              authClient.signOut({
+                fetchOptions: {
+                  onSuccess: () => {
+                    router.push("/login");
+                  },
+                },
+              })
+            }
+            className="gap-x-4 h-10 px-4"
+          >
+            <LogOutIcon className="h-4 w-4" />
+            <span>Sign Out</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
